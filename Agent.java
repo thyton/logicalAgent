755099b3456d1d311a75b7f042e553d1b1b5fe9d
Thy Ton <maithytonn@gmail.com>
2019-03-31 17:03:49 -0700

    Added some fields and scaffolding for Agent's functions

---
+++ Agent.java
@@ -0,0 +1,20 @@
+public class Agent{
+    private int points;
+    private int xx;
+    private int yy;
+    public Agent() {
+        xx = 1;
+        yy = 1;
+        points = 0;
+    }
+
+    // shootArrow: Agent shoots arrow and updates points appropriately
+    
+    // moveLeft
+
+    // moveRight
+
+    // moveUp
+
+    // moveDown
+}
